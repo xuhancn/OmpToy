692610331e91f2f979da406cf1cc78a4f337ee5c
Xu Han <xu.han@outlook.com>
2023-12-12 11:21:19 +0800

    It works.

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,13 @@
 
 int main (int argc, char **argv)
 {
+    printf("Hello OmpToy.\n");
     int a[N];
 
     #pragma omp parallel for
     for (int i = 0; i < N - 1; i++) {
         a[i] = a[i + 1];
     }
+
+    printf("Bye OmpToy.\n");
 }
